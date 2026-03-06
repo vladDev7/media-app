e771d62e33db2e06b2a0be03e81c22cf3a07d698
vladDev7 <ilchenkovlad916@gmail.com>
2026-03-06 14:17:02 +0100

    feat(auth): implement database module, add connection to database

---
+++ src/database/database.service.ts
@@ -0,0 +1,17 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { type ConfigType } from '@nestjs/config';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from 'generated/prisma/client';
+import databaseConfig from 'src/config/database.config';
+
+@Injectable()
+export class DatabaseService extends PrismaClient {
+    constructor(
+        @Inject(databaseConfig.KEY)
+        private readonly config: ConfigType<typeof databaseConfig>
+    ) {
+        // ConfigModule.
+        const adapter = new PrismaPg({ connectionString: config.url })
+        super({ adapter });
+    }
+}
