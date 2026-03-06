7da62eec7e7ed1e272dfafc3f247cae1f257a668
vladDev7 <ilchenkovlad916@gmail.com>
2026-03-06 14:20:50 +0100

    feat(auth): add DI to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,20 +2,29 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseService } from './database/database.service';
 import databaseConfig from './config/database.config';
 import configValidationSchema from './config/config.validationSchema';
+import { DatabaseModule } from './database/database.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal: true,
-    validationSchema: configValidationSchema,
-    validationOptions: {
-      abortEarly: false,
-      allowUnknown: false,
-    },
-    load: [databaseConfig],
-  })],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: configValidationSchema,
+      validationOptions: {
+        abortEarly: false,
+        // allowUnknown: false,
+      },
+      load: [databaseConfig],
+    }),
+    DatabaseModule,
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, DatabaseService],
 })
 export class AppModule {}
