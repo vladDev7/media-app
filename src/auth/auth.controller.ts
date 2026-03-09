f942d960adf5006f9b75ae6f73add70fa515c58f
vladDev7 <ilchenkovlad916@gmail.com>
2026-03-09 21:38:53 +0100

    feat(auth): implement local auth strategie

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards, Request } from '@nestjs/common';
 import { Prisma } from 'generated/prisma/client';
 import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +10,11 @@ export class AuthController {
     @Post('/')
     async register(@Body() data: Prisma.UserCreateInput) {
         return this.authService.register(data);
+    }
+
+    @UseGuards(AuthGuard('local'))
+    @Post('/login')
+    async login(@Request() req) {
+        return req.user
     }
 }
