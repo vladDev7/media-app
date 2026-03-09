6a2008b13288fd14157169680107ec1cff189113
vladDev7 <ilchenkovlad916@gmail.com>
2026-03-09 21:33:07 +0100

    feat(auth): update register logic, implement logic for validate if user exists

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { UserCreateInput } from 'generated/prisma/models';
 import { UsersService } from 'src/users/users.service';
+import { compare } from 'bcryptjs'
+import { User } from 'generated/prisma/client';
 
 @Injectable()
 export class AuthService {
@@ -9,8 +11,25 @@ export class AuthService {
         private userService: UsersService,
     ) {}
 
-    register(data: UserCreateInput) {
-        console.log(data)
+    async register(data: UserCreateInput) {
         return this.userService.create(data)
+    }
+
+    async validateUser(login: string, password: string): Promise<Omit<User, 'password'> | null> {
+        const user = await this.userService.findOne({ login });
+
+        if (!user) {
+            throw new Error('User was not found');
+        }
+
+        const isPasswordValid = await compare(password, user.password);
+
+        if (isPasswordValid) {
+            const {password: _, ...result} = user;
+
+            return result
+        }
+
+        return null;
     }
 }
