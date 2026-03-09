530cfebc4b65e7916ee629c0a9178a2ccd33c2d5
vladDev7 <ilchenkovlad916@gmail.com>
2026-03-09 21:37:31 +0100

    feat(auth): add new config, temporary solution

--- src/config/database.config.validationSchema.ts
+++ src/config/database.config.validationSchema.ts
@@ -1,8 +1,8 @@
 import * as Joi from "joi";
 
 export default Joi.object({
-    port: Joi.string(),
-    host: Joi.number().integer(),
+    host: Joi.string(),
+    port: Joi.number().integer(),
     url: Joi.string(),
     username: Joi.string(),
     password: Joi.string(),
