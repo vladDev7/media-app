3e231f83fc2f4f5597dcaa738634bb5406d6a901
vladDev7 <ilchenkovlad916@gmail.com>
2026-03-09 22:09:22 +0100

    feat(auth): implement separate local auth guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post, UseGuards, Request } from '@nestjs/common';
 import { Prisma } from 'generated/prisma/client';
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport';
+import { LocalAuthGuard } from './guards/local.auth-guard';
 
 @Controller('auth')
 export class AuthController {
@@ -12,7 +13,7 @@ export class AuthController {
         return this.authService.register(data);
     }
 
-    @UseGuards(AuthGuard('local'))
+    @UseGuards(LocalAuthGuard)
     @Post('/login')
     async login(@Request() req) {
         return req.user
