d2600e723859b8bcfe03f4f927dde49b57913ec6
vladDev7 <ilchenkovlad916@gmail.com>
2026-03-09 21:34:07 +0100

    feat(auth): update creating user logic, implement find one method

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { hash } from 'bcryptjs';
 import { Prisma, User } from 'generated/prisma/client';
 import { DatabaseService } from 'src/database/database.service';
 
@@ -9,6 +10,17 @@ export class UsersService {
     ) {}
 
     async create(data: Prisma.UserCreateInput): Promise<User> {
-        return this.databaseService.user.create({data});
+        const saltedUser = {...data, password: await hash(data.password, 10)} // TODO: update salt
+        return this.databaseService.user.create({data: saltedUser});
+    }
+
+    async findOne(data: {login: string}): Promise<User> {
+        const { login } = data;
+        const user = await this.databaseService.user.findUnique({where: {login}});
+
+        if (!user) {
+            throw new Error();
+        }
+        return user;
     }
 }
