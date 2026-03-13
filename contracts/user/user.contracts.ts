028391596fc25f9effccdb1cdca9798b310dee47
vladDev7 <ilchenkovlad916@gmail.com>
2026-03-13 16:38:31 +0100

    feat(type-improving): update user schemas and types

--- contracts/user/user.contracts.ts
+++ contracts/user/user.contracts.ts
@@ -2,19 +2,26 @@ import * as zod from 'zod';
 
 export const UserSchema = zod.object({
   id: zod.int(),
-  email: zod.email(),
+  email: zod.email().max(255),
   password: zod.string(),
-  create_at: zod.date(),
+  name: zod.string().max(255),
+  surname: zod.string().max(255),
+  created_at: zod.date(),
   updated_at: zod.date(),
 });
 
 export const UserCreateSchema = UserSchema.pick({
   email: true,
   password: true,
+  name: true,
+  surname: true,
 });
 
 export const UserLoginSchema = zod.object({
-  user: UserCreateSchema,
+  user: UserSchema.pick({
+    email: true,
+    password: true,
+  }),
 });
 
 export type UserDto = zod.infer<typeof UserSchema>;
